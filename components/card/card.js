e398586f877ca85638a02abc8f1b0088a452a7ea
Bee <ardelmbiplang@gmail.com>
2020-06-12 20:37:24 +0100

    Card component to display friends information

---
+++ components/card/card.js
@@ -0,0 +1,12 @@
+
+const Card = () => (
+  <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 pointer'>
+    <img src='https://robohash.org/set_set5/3.14159?size=100x100' />
+    <div>
+      <h2>Jane Doe</h2>
+      <p>jane.doe@gmail.com</p>
+    </div>
+  </div>
+);
+
+export default Card;
